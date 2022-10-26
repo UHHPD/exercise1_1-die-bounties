3070656128a3460ae9b3b52fa48705542b3b1445
Stella S <stella.felice.schaefer@desy.de>
2022-10-26 16:14:13 +0000

    Aufgabe 2

---
+++ summieren.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+int main () {
+  int value1, value2;
+  int sum;
+  string line;
+  ifstream inFile ("daten.txt");
+
+  ofstream outFile;
+  outFile.open ("summieren.txt");
+
+  if (inFile.is_open())
+  {
+    //write the first line to the terminal
+    while( !inFile.eof() )
+    {
+      inFile >> value1 >> value2;
+      if (inFile.eof()) {break;}
+      sum = value1+value2;
+      cout << value1 << "+" << value2 << "=" << sum << "\n";
+      outFile << sum << "\n";
+    }
+    inFile.close();
+    outFile.close();
+  }
+
+  else cout << "Unable to open file";
+
+  return 0;
+}
