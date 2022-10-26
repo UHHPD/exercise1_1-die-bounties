d7da36b381bd8b93a11c2351d00387b1b64645d2
Stella S <stella.felice.schaefer@desy.de>
2022-10-26 16:35:25 +0000

    Aufgabe 1

--- hallo.cc
+++ hallo.cc
@@ -1,26 +1,5 @@
-#include <iostream>
+# include < iostream >
 
-// excercise 1: Print hello world
-int ex1() {
-  std::cout << "Hallo Welt!" << std::endl;
+int main () {
+std :: cout << " Hallo ␣ Welt ! " << std :: endl ;
 }
-
-// excercise 2: Input and Output
-int ex2() {
-  int zahl;
-  std::cin >> zahl;
-  std::cout << zahl << std::endl;
-}
-
-int test() {
-  int a = 5;
-  std::count << "a" << std::endl;
-}
-
-int main() {
-  ex1();
-  test();
-}
-/* comment
-comment line 2
-*/
