c4cef620d05a89362fc0eeae693adc0670b4bc9a
Stella S <stella.felice.schaefer@desy.de>
2022-11-09 17:26:50 +0000

    Aufgabe 2, Blatt 3

--- likelihood.cc
+++ likelihood.cc
@@ -23,7 +23,7 @@ double prob(double mu, vector<int> data) {
 
 int main(){
   vector<int> daten;
-  vector<int> uncertainty;
+  vector<double> uncertainty;
   int value;
   ifstream inFile ("datensumme.txt");
   if (inFile.is_open())
@@ -47,11 +47,24 @@ int main(){
   	outFile1 << mean << " " << prob(mean, daten) << "\n";
   	outFile2 << mean << " " << -2*log(prob(mean, daten)) << "\n";
   	outFile3 << mean << " " << -2*log(prob(mean, daten)) + 2*log(prob(3.11538, daten)) << "\n";
-    if (-2*log(prob(mean, daten)/prob(3.11538, daten)) < 1.0) {uncertainty.push_back(mean);}
+  }
+  for (double mean = 3.0; mean <=3.3; mean += 0.01) {
+    if (-2*log(prob(mean, daten)/prob(3.11538, daten)) < 1.0) {uncertainty.push_back(mean); cout << "test " << mean << "\n";}
   }
   outFile1.close();
   outFile2.close();
   outFile3.close();
 
+  double sigma_file = 1.65365;
+  double normalize_donominator = 1.0;
+  for (int i=0; i<daten.size(); i++) {normalize_donominator *= poisson(daten[i], daten[i]);}
+  double normalized_likelihood = -2*log(prob(3.11538, daten)/normalize_donominator);
+  cout << "normalized: " << -2*log(prob(3.11538, daten)/normalize_donominator) << "\n";
   cout << "likelihood: " << prob(3.11538, daten) << "\n";
+  cout << "uncertainty on sample mean approx.: " << sigma_file/sqrt(234) << "\n";
+  cout << "uncertainty on estimator: " << (uncertainty[uncertainty.size()-1]-uncertainty[0])/2 << "\n";
+
+  //compute z
+  double z = (normalized_likelihood-233)/sqrt(2*233);
+  cout << "relative deviation likelihood ratio from mean: " << z << "\n";
 }
